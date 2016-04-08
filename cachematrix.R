0dcdc8d1b94206cb1fa0465067aed50bc19a5339
KishoreDG <dgkishore@gmail.com>
2016-04-07 22:49:52 -0400

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,82 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Goal of this assignment is demonstrate how store values 
+# using <<- operator which can be used to assign a value to an object in 
+# an environment that is different from the current environment. 
+# 
 
-## Write a short comment describing this function
+#> source("<your local path>/ProgAssign2.R")
 
-makeCacheMatrix <- function(x = matrix()) {
+#> a <- makeCacheMatrix(matrix(c(2,2,4,6),nrow=2,ncol=2))
 
+#> fcacheSolve(a)
+#[,1] [,2]
+#[1,]  1.5 -1.0
+#[2,] -0.5  0.5
+
+#> fcacheSolve(a)
+#[,1] [,2]
+#[1,]  1.5 -1.0
+#[2,] -0.5  0.5
+
+ 
+makeCacheMatrix <- function(fValue = matrix())
+{
+  
+  mCache <- NULL  ## declare a variable to hold the cache
+  ## set it to nothing (NULL)
+  
+  ## function set matrix data 
+  fSetMatrix <- function(mValue)
+  {
+    fValue <<- mValue ## new mValue is now initialized to "fValue"
+    mCache <<- NULL   ## clear the data in cache variable 
+  }
+  
+  ## function to get the data from fValue
+  fGetMatrix <- function()
+  {
+    fValue
+  }
+  
+  # set
+  fSetToCache <- function(s)
+  {
+    mCache <<- s
+  }
+  
+  fgetFromCache <- function()
+  {
+    mCache
+  }
+  
+  list(fSetMatrix = fSetMatrix, fGetMatrix = fGetMatrix, fSetToCache = fSetToCache, fgetFromCache = fgetFromCache)
+  
 }
 
 
-## Write a short comment describing this function
+## Solve Operation Function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+fcacheSolve <- function(y, ...) {
+  
+  # try to get the value from Cache using fGetInverseFromCache function 
+  mInverse <- y$fgetFromCache()
+  
+  # make sure that there is a valid value in the cache (check for not NULL)
+  # if value persists in the cache
+  # return it
+  
+  if(!is.null(mInverse)) {
+    return(mInverse)
+  }
+  
+  # if no previous value exists in the cache
+  # caclulate the inverse for the matrix (this could be the first time as well)
+  # once the calculation is done, store it in the cache 
+  # using fSetInverseToCache function
+  
+  mData <- y$fGetMatrix()
+  mInverse <- solve(mData)     ## solve function is used
+  y$fSetToCache(mInverse)
+  
+  # return the calculated inverse
+  mInverse
 }
